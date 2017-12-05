b532617e4d57c59560cf171b9267aa1767d1a111
azherittb <azher@ittblazers.com>
2017-12-05 15:53:04 +0530

    P:HyperSpot, W:Profile Edit Page

--- modules/custom/hyperspot_core/src/Form/MyProfileForm.php
+++ modules/custom/hyperspot_core/src/Form/MyProfileForm.php
@@ -28,10 +28,55 @@ class MyProfileForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['email_address'] = array(
-      '#type' => 'email',
-      '#title' => $this->t('Your Email Address')
+    $form['frist_name'] = array(
+      '#type' => 'textfield',
+      '#title' => t('First Name'),
     );
+    $form['last_name'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Last Name'),
+    );
+    $form['phone_number'] = array(
+      '#type' => 'tel',
+      '#title' => t('Phone Number'),
+    );
+    $form['e_mail'] = array(
+      '#type' => 'email',
+      '#title' => t('E- Mail'),
+    );
+    $form['address'] = array(
+      '#type' => 'textarea',
+      '#title' => $this->t('Address'),
+      '#rows' => 1,
+      '#cols' => 10,
+    );
+    $form['state'] = array(
+      '#type' => 'textfield',
+      '#title' => t('State'),
+    );
+    $form['city'] = array(
+      '#type' => 'textfield',
+      '#title' => t('City'),
+    );
+    $form['zipcode'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Zip Code'),
+    );
+    $form['country'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Country'),
+    );
+    $form['picture'] = array(
+      '#type' => 'file',
+      '#title' => t('Picture'),
+    );
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Save Changes'),
+      '#button_type' => 'primary',
+    );
+    $form['#theme'] = 'my_profile_edit';
     return $form;
   }
 
@@ -39,9 +84,8 @@ class MyProfileForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-
-    if (!filter_var($form_state->getValue('email_address', FILTER_VALIDATE_EMAIL))) {
-      $form_state->setErrorByName('email_address', $this->t('The Email Address you have provided is invalid.'));
+    if (strlen($form_state->getValue('phone_number')) < 10) {
+      $form_state->setErrorByName('phone_number', $this->t('Mobile number is too short.'));
     }
   }
 
@@ -49,7 +93,10 @@ class MyProfileForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    drupal_set_message($this->t('Your Email Address is @email', array('@email' => $form_state->getValue('email_address'))));
+    // drupal_set_message($this->t('@can_name ,Your application is being submitted!', array('@can_name' => $form_state->getValue('candidate_name'))));
+    foreach ($form_state->getValues() as $key => $value) {
+      drupal_set_message($key . ': ' . $value);
+    }
   }
 
 }
